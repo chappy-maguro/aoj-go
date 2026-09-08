68c262c85247c6a981bfaa447deee507832e8788
yuma <yuma.ito3204@gmail.com>
2026-09-08 18:50:44 +0900

    1_B

---
+++ ITP-1/1_B/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// var input float64
+	// fmt.Scan(&input)
+	// fmt.Println(math.Pow(input, 3))
+
+	var x int
+	fmt.Scan(&x)
+	fmt.Println(x * x * x)
+}
